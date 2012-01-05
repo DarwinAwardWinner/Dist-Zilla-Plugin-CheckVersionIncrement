c66683a8d03d25cb2c7ed3afc3ea790301589f9a
Ryan C. Thompson <rct@thompsonclan.org>
2012-01-05 10:43:51 -0800

    First version

--- lib/Dist/Zilla/Plugin/CheckVersionIncrement.pm
+++ lib/Dist/Zilla/Plugin/CheckVersionIncrement.pm
@@ -1,59 +1,68 @@
 ## no critic
 package Dist::Zilla::Plugin::CheckVersionIncrement;
 ## use critic
-# ABSTRACT: [1-line description of module]
+# ABSTRACT: Prevent a release unless the version number is incremented
 use Moose;
 
-# Suggestions:
-# extends 'Dist::Zilla::Plugin::???';
-# with 'Dist::Zilla::Role::???';
+with 'Dist::Zilla::Role::BeforeRelease';
+use Encode qw(encode_utf8);
+use LWP::UserAgent;
+use version ();
+use JSON::PP;
 
-# [MODULE IMPLEMENTATION GOES HERE]
+# Lots of this is cargo-culted from DZP::CheckPrereqsIndexed
+sub before_release {
+    my ($self) = @_;
+
+    my $pkg = $self->zilla->name;
+    $pkg =~ s/-/::/g;
+    ### $pkg
+
+    my $pkg_version = version->parse($self->zilla->version);
+    my $indexed_version;
+
+    my $ua = LWP::UserAgent->new(keep_alive => 1);
+    $ua->env_proxy;
+    my $res = $ua->get("http://cpanidx.org/cpanidx/json/mod/$pkg");
+    if ($res->is_success) {
+        my $yaml_octets = encode_utf8($res->decoded_content);
+        my $payload = JSON::PP->new->decode($yaml_octets);
+        if (@$payload) {
+            $indexed_version = version->parse($payload->[0]{mod_vers});
+        }
+    }
+
+    if ($indexed_version) {
+        return if $indexed_version < $pkg_version;
+        return if $self->zilla->chrome->prompt_yn(
+            "A higher version number is already indexed on CPAN. Release anyway?",
+            { default => 0 }
+        );
+        $self->log_fatal("aborting release because a higher version number is already indexed on CPAN");
+    }
+    else {
+        $self->log("Dist not indexed on CPAN. Skipping check for incremented version.");
+    }
+}
 
 1; # Magic true value required at end of module
 __END__
 
 =head1 SYNOPSIS
 
-    use Dist::Zilla::Plugin::CheckVersionIncrement;
+In your F<dist.ini>
 
-    [Sample usage of module]
+    [CheckVersionIncrement]
 
 =head1 DESCRIPTION
 
-[EXPLAIN]
+This plugin prevents your from releasing a distribution unless it has
+a version number I<greater> than the latest version already indexed on
+CPAN.
 
 =head1 BUGS AND LIMITATIONS
-
-[KNOWN BUGS]
 
 Please report any bugs or feature requests to
 C<rct+perlbug@thompsonclan.org>.
 
 =head1 SEE ALSO
-
-=for :list
-[* L<Related::Module> - How it is related]
-
-=head1 DISCLAIMER OF WARRANTY
-
-BECAUSE THIS SOFTWARE IS LICENSED FREE OF CHARGE, THERE IS NO WARRANTY
-FOR THE SOFTWARE, TO THE EXTENT PERMITTED BY APPLICABLE LAW. EXCEPT WHEN
-OTHERWISE STATED IN WRITING THE COPYRIGHT HOLDERS AND/OR OTHER PARTIES
-PROVIDE THE SOFTWARE "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER
-EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE
-ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE SOFTWARE IS WITH
-YOU. SHOULD THE SOFTWARE PROVE DEFECTIVE, YOU ASSUME THE COST OF ALL
-NECESSARY SERVICING, REPAIR, OR CORRECTION.
-
-IN NO EVENT UNLESS REQUIRED BY APPLICABLE LAW OR AGREED TO IN WRITING
-WILL ANY COPYRIGHT HOLDER, OR ANY OTHER PARTY WHO MAY MODIFY AND/OR
-REDISTRIBUTE THE SOFTWARE AS PERMITTED BY THE ABOVE LICENCE, BE
-LIABLE TO YOU FOR DAMAGES, INCLUDING ANY GENERAL, SPECIAL, INCIDENTAL,
-OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OR INABILITY TO USE
-THE SOFTWARE (INCLUDING BUT NOT LIMITED TO LOSS OF DATA OR DATA BEING
-RENDERED INACCURATE OR LOSSES SUSTAINED BY YOU OR THIRD PARTIES OR A
-FAILURE OF THE SOFTWARE TO OPERATE WITH ANY OTHER SOFTWARE), EVEN IF
-SUCH HOLDER OR OTHER PARTY HAS BEEN ADVISED OF THE POSSIBILITY OF
-SUCH DAMAGES.
